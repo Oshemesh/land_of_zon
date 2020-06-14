7e3ac247c7f6f370809030540a9d3960ee15e32c
leeorshemesh <66887266+leeorshemesh@users.noreply.github.com>
2020-06-14 00:10:07 -0700

    animation run left right jump

--- worldofzon.py
+++ worldofzon.py
@@ -16,7 +16,7 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))
 clock = pygame.time.Clock()
 
 #load graphics
-background = pygame.image.load( os.getcwd() + r"\\Battleground4.png")
+background = pygame.image.load( os.getcwd() + r"\\images\\Battleground4.png")
 font_name = pygame.font.match_font("Pokemon GB.ttf", 32)
 
 def draw_text(text, size, color, x, y):
@@ -26,6 +26,14 @@ def draw_text(text, size, color, x, y):
     screen.blit(text_surface, text_rect)
 
 class Player(pygame.sprite.Sprite):
+    idleCounter = 0
+    currentlyFacingRight = True
+    goingUp = True
+    highestPoint = False
+    isJumping = False
+    isAnimating = False # IDLE DOES NOT COUNT
+    animationCounter = 0
+    facingRight = False
     speed = 0
     max_health = 0
     current_health = 0
@@ -33,51 +41,169 @@ class Player(pygame.sprite.Sprite):
     defense = 0
     attack_speed = 0
     
-    char_img_l = pygame.Surface((500, 40))
-    char_img_r = pygame.Surface((500, 40))
+    current_image = pygame.Surface((500, 40))
         
     
-    walk_array_right = []
+    run_array_right = []
+    run_array_left = [] 
     
-    def __init__(self , speed, max_h , attack, defense, attack_s, char_img_l, char_img_r):
+    jump_array_right = []
+    jump_array_left = []
+    
+    idle_array = []
+ 
+    def __init__(self , speed, max_h , attack, defense, attack_s, idle_img_array, run_img_array, jump_img_array, starting_img, facingRight):
+        self.facingRight = facingRight
         pygame.sprite.Sprite.__init__(self)
         self.max_health = max_h
         self.current_health = max_h
         self.attack = attack
         self.defense = defense
         self.attack_speed = attack_s
-        self.image = pygame.Surface((500, 40))
-        x = pygame.image.load(char_img_r)
-        x = pygame.transform.scale(x, (200, 130))
-        self.image = x
-        self.char_img_r = x;
+        self.current_image = starting_img
+        self.image = starting_img
         
-        y = pygame.image.load(char_img_l)
-        y = pygame.transform.scale(y, (200, 130))
-        self.char_img_l = y
-        #self.image.fill((55, 121, 179))
+        self.jump_array = jump_img_array
+        self.idle_array = idle_img_array
+        
+        #Load left and right running animation imgs
+        if (facingRight):
+            self.run_array_right = run_img_array
+            self.jump_array_right = jump_img_array
+            
+            for img in run_img_array:
+                self.run_array_left.append( pygame.transform.flip( img , True, False) )
+            for img in jump_img_array:
+                self.jump_array_left.append( pygame.transform.flip( img , True, False) )
+                      
+        else:
+            self.run_array_left = run_img_array
+            self.jump_array_left = jump_img_array
+            for img in run_img_array:
+                self.run_array_right.append( pygame.transform.flip( img , True, False) )       
+            for img in jump_img_array:
+                self.jump_array_right.append( pygame.transform.flip( img , True, False) ) 
+                
         self.rect = self.image.get_rect()
         self.radius = 19
         self.speed = speed
-        #pygame.draw.circle(self.image, (255, 0, 0), self.rect.center, self.radius)
         self.rect.center = (50, 300)
+    # THIS GETS CALLED EVERY TIME all_sprites.update() happens
     def update(self):
+        idleState = True   
         self.vx = 0
         keystate = pygame.key.get_pressed()
-        if keystate[pygame.K_RIGHT]:
-            self.vx += self.speed
-            self.image = self.char_img_r;
-        if keystate[pygame.K_LEFT]:
-            self.vx -= self.speed
-            self.image = self.char_img_l
-        if keystate[pygame.K_UP]:
-            if( self.rect.y >= 420):
-                if(self.rect.y >= 20):
-                    self.rect.y -= self.speed*15
-        self.rect.x += self.vx 
+        #Moving to the right
         
-        if(self.rect.y < 420 ):
-            self.rect.y += 4
+        if(self.isJumping):
+            jump_imgs = []
+            if(self.currentlyFacingRight):
+                jump_imgs = self.jump_array_right
+            else:
+                jump_imgs = self.jump_array_left
+            
+            self.isAnimating = True
+            self.animationCounter += 1
+            if self.animationCounter < 5:
+                self.image = jump_imgs[0]
+
+            elif self.animationCounter < 10:
+                self.image = jump_imgs[1]
+              
+            elif self.animationCounter < 15:
+                self.image = jump_imgs[2]
+         
+            elif self.animationCounter < 20:
+                self.image = jump_imgs[3]
+                self.highestPoint = True
+                         
+            elif self.animationCounter < 25:
+                self.highestPoint = False
+                self.goingUp = False
+                self.image = jump_imgs[4]
+            elif self.animationCounter < 30:
+                self.image = jump_imgs[5]
+            elif self.animationCounter < 35:
+                self.image = jump_imgs[6]
+            elif self.animationCounter < 40:
+                self.isJumping = False
+                self.animationCounter = 0;  
+                self.goingUp = True
+        
+        
+        if keystate[pygame.K_UP]: 
+            idleState = False
+            if(self.isJumping == False):
+                self.animationCounter = 0
+            self.isJumping = True
+        if keystate[pygame.K_RIGHT]:
+            idleState = False
+            self.currentlyFacingRight = True
+            self.vx += self.speed
+            if(not self.isJumping):
+                self.isAnimating = True
+                self.animationCounter += 1
+                if self.animationCounter < 5:
+                    self.image = self.run_array_right[0]
+                elif self.animationCounter < 10:
+                    self.image = self.run_array_right[1]
+                elif self.animationCounter < 15:
+                    self.image = self.run_array_right[2]
+                elif self.animationCounter < 20:
+                    self.image = self.run_array_right[3]
+                elif self.animationCounter < 25:
+                    self.image = self.run_array_right[4]
+                elif self.animationCounter < 30:
+                    self.image = self.run_array_right[5]
+                elif self.animationCounter < 35:
+                    self.image = self.run_array_right[6]
+                elif self.animationCounter < 40:
+                    self.image = self.run_array_right[7]                
+                elif self.animationCounter < 45:
+                    self.image = self.current_image 
+                    self.animationCounter = 0;
+                
+        elif keystate[pygame.K_LEFT]:
+            idleState = False
+            self.currentlyFacingRight = False
+            self.vx -= self.speed
+            if(not self.isJumping):
+                self.isAnimating = True
+                self.animationCounter += 1
+                if self.animationCounter < 5:
+                    self.image = self.run_array_left[0]
+                elif self.animationCounter < 10:
+                    self.image = self.run_array_left[1]
+                elif self.animationCounter < 15:
+                    self.image = self.run_array_left[2]
+                elif self.animationCounter < 20:
+                    self.image = self.run_array_left[3]
+                elif self.animationCounter < 25:
+                    self.image = self.run_array_left[4]
+                elif self.animationCounter < 30:
+                    self.image = self.run_array_left[5]
+                elif self.animationCounter < 35:
+                    self.image = self.run_array_left[6]
+                elif self.animationCounter < 40:
+                    self.image = self.run_array_left[7]                
+                elif self.animationCounter < 45:
+                    self.animationCounter = 0;
+   
+        
+               
+        
+        self.rect.x += self.vx 
+        if(self.isJumping):
+            if not self.highestPoint:
+                if(self.goingUp):
+                    self.rect.y -= self.speed
+                else:
+                    self.rect.y += self.speed
+       
+
+        if(not self.isJumping):
+            if(self.rect.y < 420 ):
+                self.rect.y += 4
         
         if(self.rect.y >= 420):
             self.rect.y = 420
@@ -89,21 +215,55 @@ class Player(pygame.sprite.Sprite):
         
 all_sprites = pygame.sprite.Group()
 
-rogue_left_path = os.getcwd() + r"\rogue_left.png"
-rogue_right_path = os.getcwd() + r"\rogue_right.png"
 
-mage_left_path = os.getcwd() + r"\mage_left.png"
-mage_right_path = os.getcwd() + r"\mage_right.png"
-
-viking_left_path = os.getcwd() + r"\viking_left.png"
-viking_right_path = os.getcwd() + r"\viking_right.png"
+rogue_run_animation = [ pygame.image.load(os.getcwd() + r"\images\Rogue\Run\run1.png").convert_alpha() , \
+                          pygame.image.load(os.getcwd() + r"\images\Rogue\Run\run2.png").convert_alpha() , \
+                          pygame.image.load(os.getcwd() + r"\images\Rogue\Run\run3.png").convert_alpha() , \
+                          pygame.image.load(os.getcwd() + r"\images\Rogue\Run\run4.png").convert_alpha() , \
+                          pygame.image.load(os.getcwd() + r"\images\Rogue\Run\run5.png").convert_alpha() , \
+                          pygame.image.load(os.getcwd() + r"\images\Rogue\Run\run6.png").convert_alpha() , \
+                          pygame.image.load(os.getcwd() + r"\images\Rogue\Run\run7.png").convert_alpha() , \
+                          pygame.image.load(os.getcwd() + r"\images\Rogue\Run\run8.png").convert_alpha() ]
+                          
+rogue_jump_animation = [ pygame.image.load(os.getcwd() + r"\images\Rogue\Jump\jump1.png").convert_alpha() , \
+                          pygame.image.load(os.getcwd() + r"\images\Rogue\Jump\jump2.png").convert_alpha() , \
+                          pygame.image.load(os.getcwd() + r"\images\Rogue\Jump\jump3.png").convert_alpha() , \
+                          pygame.image.load(os.getcwd() + r"\images\Rogue\Jump\jump4.png").convert_alpha() , \
+                          pygame.image.load(os.getcwd() + r"\images\Rogue\Jump\jump5.png").convert_alpha() , \
+                          pygame.image.load(os.getcwd() + r"\images\Rogue\Jump\jump6.png").convert_alpha() , \
+                          pygame.image.load(os.getcwd() + r"\images\Rogue\Jump\jump7.png").convert_alpha() ]
+ 
+rogue_idle_animation = [  pygame.image.load(os.getcwd() + r"\images\Rogue\Idle\idle1.png").convert_alpha() , \
+                          pygame.image.load(os.getcwd() + r"\images\Rogue\Idle\idle2.png").convert_alpha() , \
+                          pygame.image.load(os.getcwd() + r"\images\Rogue\Idle\idle3.png").convert_alpha() , \
+                          pygame.image.load(os.getcwd() + r"\images\Rogue\Idle\idle4.png").convert_alpha() , \
+                          pygame.image.load(os.getcwd() + r"\images\Rogue\Idle\idle5.png").convert_alpha() , \
+                          pygame.image.load(os.getcwd() + r"\images\Rogue\Idle\idle6.png").convert_alpha() , \
+                          pygame.image.load(os.getcwd() + r"\images\Rogue\Idle\idle7.png").convert_alpha() , \
+                          pygame.image.load(os.getcwd() + r"\images\Rogue\Idle\idle8.png").convert_alpha() , \
+                          pygame.image.load(os.getcwd() + r"\images\Rogue\Idle\idle9.png").convert_alpha() , \
+                          pygame.image.load(os.getcwd() + r"\images\Rogue\Idle\idle10.png").convert_alpha() , \
+                          pygame.image.load(os.getcwd() + r"\images\Rogue\Idle\idle11.png").convert_alpha() , \
+                          pygame.image.load(os.getcwd() + r"\images\Rogue\Idle\idle12.png").convert_alpha() , \
+                          pygame.image.load(os.getcwd() + r"\images\Rogue\Idle\idle13.png").convert_alpha() , \
+                          pygame.image.load(os.getcwd() + r"\images\Rogue\Idle\idle14.png").convert_alpha() , \
+                          pygame.image.load(os.getcwd() + r"\images\Rogue\Idle\idle15.png").convert_alpha() , \
+                          pygame.image.load(os.getcwd() + r"\images\Rogue\Idle\idle16.png").convert_alpha() , \
+                          pygame.image.load(os.getcwd() + r"\images\Rogue\Idle\idle17.png").convert_alpha()]
+                          
+for img in rogue_run_animation:
+    img = pygame.transform.scale(img, (200, 130))
+for img in rogue_jump_animation:
+    img = pygame.transform.scale(img, (200, 130))
+for img in rogue_idle_animation:
+    img = pygame.transform.scale(img, (200, 130))    
 
 #speed, max_h , attack, defense, attack_s):
-rogue = Player(HIGH, 50, MEDIUM, LOW, HIGH , rogue_left_path, rogue_right_path )
-mage = Player(MEDIUM, 100, HIGH, LOW, MEDIUM , mage_left_path, mage_right_path)
-viking = Player(LOW, 150, HIGH, HIGH, LOW , viking_left_path, viking_right_path)
+rogue = Player(LOW + 2, 50, MEDIUM, LOW, HIGH , rogue_idle_animation , rogue_run_animation , rogue_jump_animation , rogue_run_animation[0] , True )
+#mage = Player(MEDIUM, 100, HIGH, LOW, MEDIUM , mage_left_path, mage_right_path)
+#viking = Player(LOW, 150, HIGH, HIGH, LOW , viking_left_path, viking_right_path)
 
-selectPlayer = [rogue, mage, viking]
+selectPlayer = [rogue]#, mage, viking]
 
 characterChosen = int(input("Choose your character! : (0-rogue, 1-mage, 2-viking)"))
 
@@ -128,3 +288,43 @@ while running:
     draw_text("HEALTH: " + str(selectPlayer[characterChosen].current_health) + "/" + str(selectPlayer[characterChosen].max_health) , 20,  (255, 0, 0) , 60 , 10 )
     pygame.display.flip() # do this last
 pygame.quit()
+
+
+#if(idleState):
+ #           self.idleCounter += 1
+  #          if(not self.isJumping and len(self.idle_array) >= 17 ): 
+   #             if self.idleCounter < 5:
+   #                 self.image = self.idle_array[0]
+   #             if self.idleCounter < 10:
+   #                 self.image = self.idle_array[1]
+   #             if self.idleCounter < 15:
+   #                 self.image = self.idle_array[2]
+   ##             if self.idleCounter < 20:
+   #                 self.image = self.idle_array[3]
+   #             if self.idleCounter < 25:
+   #                 self.image = self.idle_array[4]
+   #             if self.idleCounter < 30:
+   #                 self.image = self.idle_array[5]
+   #             if self.idleCounter < 35:
+   #                 self.image = self.idle_array[6]
+   #             if self.idleCounter < 40:
+   #                 self.image = self.idle_array[7]
+   #             if self.idleCounter < 45:
+   #                 self.image = self.idle_array[8]
+   #             if self.idleCounter < 50:
+   #                 self.image = self.idle_array[9]
+   ##             if self.idleCounter < 55:
+    #                self.image = self.idle_array[10]
+    #            if self.idleCounter < 60:
+    #                self.image = self.idle_array[11]
+    #            if self.idleCounter < 65:
+    #                self.image = self.idle_array[12]
+    #            if self.idleCounter < 70:
+    #                self.image = self.idle_array[13]
+    #            if self.idleCounter < 75:
+    #                self.image = self.idle_array[14]
+    #            if self.idleCounter < 80:
+    #                self.image = self.idle_array[15]
+    #            if self.idleCounter < 85:
+    #                self.image = self.idle_array[16]                    
+    #            self.idleCounter = 0
